afcb38951014af21649671ff7d2c9a77f12814c8
Burt Rosenberg <burt@cs.miami.edu>
2025-10-04 19:58:33 -0400

    ok

---
+++ threads/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(s string) {
+	fmt.Print(s)
+}
+
+func main() {
+	hello := [5]string{"h","e","l","l","o"}
+	i := 0
+	for i<len(hello) {    // or: i=0; i<5; i+=1 (no parens)
+		go f(hello[i])
+		i = i+1
+	}
+	time.Sleep(time.Second)
+	fmt.Println("")
+}
+
