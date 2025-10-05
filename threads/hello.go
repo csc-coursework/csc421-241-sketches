96fc4496dac15bb19856ba87fd48dce460ceafae
Burt Rosenberg <burt@cs.miami.edu>
2025-10-04 20:00:59 -0400

    ok

--- threads/hello.go
+++ threads/hello.go
@@ -11,10 +11,10 @@ func f(s string) {
 
 func main() {
 	hello := [5]string{"h","e","l","l","o"}
-	i := 0
+	i := 0 // := to define i, infering type
 	for i<len(hello) {    // or: i=0; i<5; i+=1 (no parens)
 		go f(hello[i])
-		i = i+1
+		i = i+1 // i updated, not defined
 	}
 	time.Sleep(time.Second)
 	fmt.Println("")
